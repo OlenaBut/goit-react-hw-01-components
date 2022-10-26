4090f337f056e3012b0049bfc8e14aab8df58bcb
Olena <lodki-omega@ukr.net>
2022-10-27 01:23:35 +0200

    prfl

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import { Profile } from './SocialProfile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './Friends/FriendList';
-import { Transactions } from './Transactions/TransactionHistory';
+
+
 
 import user from './Data/user.json';
-import data from './Data/data.json';
-import friends from './Data/friends.json';
-import transactions from './Data/transactions.json';
+
+
 
 
 export const App = () => {
@@ -19,9 +17,8 @@ export const App = () => {
       avatar={user.avatar}
       stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <Transactions items={transactions} />
+
+     
     </div>
   );
 };
